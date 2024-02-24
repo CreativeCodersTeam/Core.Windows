72d95bfd8a9d57e1fe89e5045be2a850e4316144
darthsharp <48331467+darthsharp@users.noreply.github.com>
2024-02-24 13:50:33 +0100

    Remove WPF task dialog implementation
    The WPF implementation of the task dialog has been removed from the CreativeCoders.Mvvm library. The removal includes the service and helper classes and their related resources. Several project references to CreativeCoders.Core have been replaced with a corresponding package reference.

--- samples/WindowsSampleApp/Program.cs
+++ samples/WindowsSampleApp/Program.cs
@@ -1,6 +1,7 @@
-﻿using System.Threading.Tasks;
-using CreativeCoders.Core.IO;
-using CreativeCoders.IO.Archives;
+﻿using System;
+using System.Threading.Tasks;
+using CreativeCoders.Core.Collections;
+using CreativeCoders.Windows.Window;
 
 namespace WindowsSampleApp;
 
@@ -8,39 +9,14 @@ public static class Program
 {
     public static async Task Main()
     {
-        var outputStream = FileSys.File.Create(@"c:\temp\test1.tar.gz");
+        var win32Windows = new Win32Windows();
 
-        var tarArchiveWriter = new DefaultTarArchiveWriterBuilder()
-            .PreserveFileMode()
-            .WithOwnerAndGroup()
-            .WithGZip()
-            .Build(outputStream);
+        var windows = win32Windows.EnumerateWindows();
 
-        await tarArchiveWriter.AddFromDirectoryAsync(@"c:\temp\simbasrv", @"c:\temp\").ConfigureAwait(false);
-
-        await tarArchiveWriter.DisposeAsync().ConfigureAwait(false);
-
-        //return;
-
-/*
-        var archiveCreator = new TarArchiveCreator()
-            .SetArchiveFileName(@"c:\temp\test.tar")
-            .AddFromDirectory(@"c:\temp\simbasrv", "*.*", true);
-
-        archiveCreator.Create();
-
-        archiveCreator
-            .SetArchiveFileName(@"c:\temp\test.tar.gz")
-            .Create(true);
-
-        // var win32Windows = new Win32Windows();
-        //
-        // var windows = win32Windows.EnumerateWindows();
-        //
-        // windows.ForEach(x => Console.WriteLine($"{x.WindowHandle}: {x.Title}"));
+        windows.ForEach(x => Console.WriteLine($"{x.WindowHandle}: {x.Title}"));
 
         Console.WriteLine("Press key to exit...");
         Console.ReadKey();
-*/
+
     }
 }
